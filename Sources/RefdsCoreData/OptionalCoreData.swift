9e071dbe33944626d75c9b544072169cb90f93c5
Rafael Escaleira <rafaelescaleira@icloud.com>
2023-09-19 18:13:38 -0400

    Update OptionalCoreData.swift

--- Sources/RefdsCoreData/OptionalCoreData.swift
+++ Sources/RefdsCoreData/OptionalCoreData.swift
@@ -1,29 +1,20 @@
 import Foundation
 import CoreData
 
-@propertyWrapper public struct OptionalCoreData<Value> {
-    private let key: String
-    private let object: NSManagedObject
-
-    public var wrappedValue: Value? {
-        get {
-            object.willAccessValue(forKey: key)
-            defer { object.didAccessValue(forKey: key) }
-            return object.primitiveValue(forKey: key) as? Value
-        }
-        set {
-            object.willChangeValue(forKey: key)
-            defer { object.didChangeValue(forKey: key) }
-            guard let value = newValue else {
-                object.setPrimitiveValue(nil, forKey: key)
-                return
-            }
-            object.setPrimitiveValue(value, forKey: key)
-        }
+public extension NSManagedObject {
+    func get<Value>(_ key: String) -> Value? {
+        willAccessValue(forKey: key)
+        defer { didAccessValue(forKey: key) }
+        return primitiveValue(forKey: key) as? Value
     }
     
-    public init(_ key: String, _ object: NSManagedObject) {
-        self.key = key
-        self.object = object
+    func set<Value>(_ key: String, newValue: Value?) {
+        willChangeValue(forKey: key)
+        defer { didChangeValue(forKey: key) }
+        guard let value = newValue else {
+            setPrimitiveValue(nil, forKey: key)
+            return
+        }
+        setPrimitiveValue(value, forKey: key)
     }
 }
